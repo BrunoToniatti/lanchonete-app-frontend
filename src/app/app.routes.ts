ad67749b88ce1383f7d95450bc6edc03f46de0b6
João Pedro Moreira <joao.p.moreia@gmail.com>
2025-08-14 00:04:00 -0300

    criando rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Intro } from './pages/intro/intro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: "",
+    component: Intro
+    }
+];
+// You can add your application routes here
